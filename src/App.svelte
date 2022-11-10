f5093076693a1784f762c10e727e34f9e837f1a1
David Liebemann <david@crystalmesh.de>
2022-11-10 10:25:49 +0100

    Updates

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import BudgetCalculator from './lib/BudgetCalculator.svelte';
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <BudgetCalculator />
+</main>
+
+
